250ec60bbf1c4daf9b7d31307ea39ec11f4e1307
astapi <jyunichi.k@gmail.com>
2017-04-06 21:46:03 +0900

    GIDなかった　

--- docker/bundle.sh
+++ docker/bundle.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ENV=$RACK_ENV
 export uid=$UID
-export gid=$GID
+export gid=`id -g`
 
 if [ "$ENV" = 'production' ]; then
   docker-compose run --entrypoint=$entrypoint -e RACK_ENV=$ENV web s
@@ -14,9 +14,9 @@ if [ "$1" = 'up' ]; then
 elif [ "$1" = 'rspec' ]; then
   docker-compose run --entrypoint=$entrypoint -e RACK_ENV=test web $@
 elif [ "$1" = 'spring' ]; then
-  docker-compose run -u "$UID:$GID" --entrypoint=$entrypoint -d spring spring
+  docker-compose run -u "$uid:$gid" --entrypoint=$entrypoint -d spring spring
 elif [ "$1" = 's' ]; then
   docker-compose run -p 3000:3000 -u "" --entrypoint=$entrypoint -e RACK_ENV=development web s
 else
-  docker-compose run -u "$UID:$GID" --entrypoint=$entrypoint -e RACK_ENV=development web $@
+  docker-compose run -u "$uid:$gid" --entrypoint=$entrypoint -e RACK_ENV=development web $@
 fi
